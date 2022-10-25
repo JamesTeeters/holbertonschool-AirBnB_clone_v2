c63c2d565ccdc5a9ea9498419ec6657991dd3ca0
JamesTeeters <4925@holbertonstudents.com>
2022-10-25 14:16:18 -0500

    'added DOC'

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- creating Database and user of hbnb
 CREATE DATABASE IF NOT EXSITS hbnb_dev_db;
 CREATE USER IF NOT EXSITS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
