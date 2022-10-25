baf5f7b7392a5cf0faa459ef277a08ee4056859e
JamesTeeters <4925@holbertonstudents.com>
2022-10-25 14:50:23 -0500

    ''

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
--- creating Database and user of hbnb
-CREATE DATABASE IF NOT EXSITS hbnb_dev_db;
-CREATE USER IF NOT EXSITS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- create database and user for hbnb
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER ID NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_de_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost;'
