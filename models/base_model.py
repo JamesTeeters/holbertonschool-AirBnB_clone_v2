d596253bb393d3a51e9075774f42575a9a93b9f5
actatum88 <alexandra.c.tatum@gmail.com>
2022-10-28 13:42:32 -0500

    Fixing base_model pathway

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ from datetime import date, datetime
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
 
 
 class BaseModel:
