10affd894fa667f1dd02c208fc2b6c00981dc8fc
actatum88 <alexandra.c.tatum@gmail.com>
2022-10-30 21:13:56 -0500

    Task 6 __init__ & db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a class to manage DB storage for hbnb clone"""
-import os
+"""defines a class to manage database storage"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import BaseModel, Base
+from os import getenv
+from models.base_model import Base
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -12,62 +12,57 @@ from models.amenity import Amenity
 from models.review import Review
 
 
-class DBStorage:
-    """Class for DBStorage"""
+mapped_classes = (City, State, User, Place, Amenity, Review)
 
+
+class DBStorage:
+    """Class for database storage"""
     __engine = None
     __session = None
 
     def __init__(self):
-        usr = os.getenv("HBNB_MYSQL_USER")
-        pwd = os.getenv("HBNB_MYSQL_PWD")
-        host = os.getenv("HBNB_MYSQL_HOST")
-        db = os.getenv("HBNB_MYSQL_DB")
-        env = os.getenv('HBNB_MYSQL_ENV')
-        db_url = "mysql+mysqldb://{}:{}@{}/{}".format(usr, pwd, host, db)
-
-        self.__engine = create_engine(db_url, pool_pre_ping=True)
-
-        if os.getenv("HBNB_ENV") == "test":
-            Base.metadata.drop_all(bind=self.__engine)
+        """initializes the storage"""
+        usr = getenv('HBNB_MYSQL_USER')
+        pwd = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            usr, pwd, host, db), pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """return all objects in DBStorage session"""
-        classes = ["User", "State", "City", "Place", "Amenity", "Review"]
-        new_dict = {}
-
-        if cls is None:
-            obj_list = []
-
-            for cls in classes:
-                obj_list.extend(self.__session.query(cls).all())
-            for obj in obj_list:
-                key = obj.__class__.__name__ + '.' + obj.id
-                new_dict[key] = obj
-
-        else:
-            for obj in self.__session.query(DBStorage.classes[cls].all()):
-                key = obj.__class__.__name__ + '.' + obj.id
-                new_dict[key] = obj
-
-        return new_dict
+        """Returns a dictionaary of all objects in DB"""
+        objs = {}
+        if cls in mapped_classes:
+            objs.update({"{}.{}".format(cls.__name__, item.id): item
+                         for item in self.__session.query(cls)})
+        elif cls is None:
+            for c in mapped_classes:
+                objs.update({"{}.{}".format(c.__name__, item.id): item
+                             for item in self.__session.query(c)})
+        return objs
 
     def new(self, obj):
-        """add new object to DBStorage session"""
-        self.__session.add(obj)
+        """adds an object to the session"""
+        if type(obj) in mapped_classes:
+            self.__session.add(obj)
 
     def save(self):
-        """commit all changes to DBStorage session"""
+        """commits the changes in  the current session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ delete instance in DBStorage session"""
-        if obj is None:
-            return
-        else:
+        """deletes an object from the session"""
+        if obj in self.all(type(obj).values()):
             self.__session.delete(obj)
 
-    def reload(self, obj=None):
+    def reload(self):
+        """reloads from the database"""
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(bind=self.__engine,
                                                      expire_on_commit=False))
+
+    def close(self):
+        """closes the current session"""
+        self.__session.remove()
