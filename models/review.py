15e8c03155d0b6665bb4c0322b8e70d78bcb5bc5
actatum88 <alexandra.c.tatum@gmail.com>
2022-10-30 22:27:23 -0500

    Task 9 and debugging

--- models/review.py
+++ models/review.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models.user import User
+import os
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'),
+                      nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'),
+                     nullable=False)
+    text = Column(String(1024), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+        @property
+        def user(self):
+            """gets the user who made the review"""
+            from models import storage
+            return storage.all(User).get("User.{}".format(self.user_id))
