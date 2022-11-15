f75070e31207fae4e8097a76974b8f8165bcf9b5
james teeters <teeters@Holby>
2022-11-15 12:37:11 -0600

    display number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"a script that starts a Flask web application"
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_HBNB():
+    """
+    prints Hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def HBNB():
+    """
+    prints HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def C(text):
+    text = text.replace("_", " ")
+    return ("C {}".format(text))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def P_text(text="is cool"):
+    """
+    displays Python followed by User input
+    default Python is cool
+    """
+    text = text.replace("_", " ")
+    return ("Python {}".format(text))
+
+
+@app.route("/number/<n>")
+def number(n):
+    """
+    displays number if integer
+    """
+    if n.isdigit():
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
