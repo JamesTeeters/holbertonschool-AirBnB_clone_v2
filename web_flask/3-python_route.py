de86854c5befd38771b5aced060c66cf34761626
james teeters <teeters@Holby>
2022-11-15 12:20:19 -0600

    test

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -37,12 +37,15 @@ def P_is_cool():
     """
     return "Python is cool"
 
-"""
+
 @app.route("/python/<text>")
-def P_is_cool(text="is cool"):
+def P_text(text="is cool"):
+    """
+    displays Python followed by User input
+    """
     text = text.replace("_", " ")
     return f"Python {text}"
-"""
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
