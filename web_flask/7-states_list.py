6832bc97b8674d786ed608bc707175b200a9329a
james teeters <teeters@Holby>
2022-11-16 14:58:53 -0600

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,7 +14,7 @@ def states_list():
     """
     show a list of states
     """
-    all_states = storage.all("State").values()
+    all_states = storage.all(State)
     return render_template('7-states_list.html', all_states=all_states)
 
 
